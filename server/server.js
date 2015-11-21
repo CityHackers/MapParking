5e4d555a7ed973838154c5796c3da977870a7840
Ting Deng <maidenbelize@gmail.com>
2015-11-21 14:38:53 -0800

    merged server with index.html

--- server/server.js
+++ server/server.js
@@ -8,11 +8,6 @@ var app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-
 app.use(express.static(__dirname + '/../client'));
-
-app.get('/', function(req,res){
-  res.send('Hello City Hackers!');
-});
 
 app.listen(process.env.PORT || 3000);
