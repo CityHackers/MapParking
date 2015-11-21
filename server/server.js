2e7ee3b0016c978280adab2bb4b21942e374f282
tbutman <tbutman@gmail.com>
2015-11-21 15:47:58 -0800

    [feat] firebase integration

--- server/server.js
+++ server/server.js
@@ -1,13 +1,24 @@
-var express = require('express');
-var cors = require('cors');
-var bodyParser = require('body-parser');
+var express = require("express");
+var Firebase = require('firebase');
+var firebase = new Firebase('https://tatertots.firebaseio.com/');
 
+var port = process.env.port || 4000;
 
 var app = express();
 
-app.use(cors());
-app.use(bodyParser.json());
+// URL syntax: http://host/path?id={device}&time={time}&key1={var1}&key2={var2}... 
+// Available variables: device, time, duplicate, snr, station, data, avgSignal, lat, lng, rssi, seqNumber
+app.get('/data', function(req, res){
+  var params = req.query;
+  console.log(params)
+  firebase.child("parkingSpots").push(params);
 
-app.use(express.static(__dirname + '/../client'));
+  res.send(JSON.stringify(params));
+});
 
-app.listen(process.env.PORT || 3000);
+var server = app.listen(port, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Listening at http://%s:%s', host, port);
+});
