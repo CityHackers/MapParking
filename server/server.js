744da2f93be6c8d6753a631c4b124573b0eb8dca
gtdeng <gt.deng@gmail.com>
2015-11-22 15:54:54 -0800

    deploy

--- server/server.js
+++ server/server.js
@@ -2,13 +2,13 @@ var express = require("express");
 // var Firebase = require('firebase');
 // var firebase = new Firebase('https://tatertots.firebaseio.com/');
 
-var port = process.env.port || 3000;
+var port = process.env.PORT || 3000;
 
 var app = express();
 
 app.use(express.static(__dirname + '/../client'));
 
-// URL syntax: http://host/path?id={device}&time={time}&key1={var1}&key2={var2}... 
+// URL syntax: http://host/path?id={device}&time={time}&key1={var1}&key2={var2}...
 // Available variables: device, time, duplicate, snr, station, data, avgSignal, lat, lng, rssi, seqNumber
 app.get('/data', function(req, res){
   var params = req.query;
